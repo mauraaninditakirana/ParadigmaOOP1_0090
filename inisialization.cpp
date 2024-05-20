d0a381e670aa674d212cedd550240cb7582cd91e
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-20 14:38:41 +0700

    Membuat akses modifier public

--- inisialization.cpp
+++ inisialization.cpp
@@ -1,6 +1,22 @@
 #include <iostream> 
 using namespace std;
 
-class Mahasiswa {
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
 
+public: 
+    mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) 
+    {
+        //Member Initialization List
+        //Definisi
+    }
+    ~mahasiswa(){
+        cout << "id = " << id << endl;
+        cout << "Nama = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
 };
+
