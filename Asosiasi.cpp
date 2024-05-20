f0750069290272a0b6a82d6d41afdd780341d659
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-20 18:07:26 +0700

    membuat void dokter cetak dan tambah pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -38,5 +38,16 @@ void pasien::cetakDokter() {
     for (auto& a : daftar_dokter) {
         cout << a->nama << "\n";
     }
-    cout endl;
+    cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien(){
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
 }
